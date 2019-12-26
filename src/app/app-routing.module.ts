1f1b5c23e056a2bf8d0ab77c5bac06d33cdf7a34
Qu1oX <danny1996@gmx.net>
2019-12-26 17:18:39 +0100

    Added company-login, employee-login and home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EmployeeLoginComponent} from './employee-login/employee-login.component';
+import {CompanyLoginComponent} from './company-login/company-login.component';
+import {HomeComponent} from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Home
+  {
+    path: '',
+    component: HomeComponent
+  },
+  // Employee Login
+  {
+    path: 'employee-login',
+    component: EmployeeLoginComponent
+  },
+  // Company Login
+  {
+    path: 'company-login',
+    component: CompanyLoginComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CompanyLoginComponent} from './company-login/company-login.component';
+import {EmployeeLoginComponent} from './employee-login/employee-login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyLoginComponent,
+    EmployeeLoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
