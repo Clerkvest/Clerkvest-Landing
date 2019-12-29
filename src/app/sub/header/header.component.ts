858209603fa96088f00b519f3583b0e6f9d4d919
Qu1oX <danny1996@gmx.net>
2019-12-29 23:03:19 +0100

    Added timer to switch keywords

--- src/app/sub/header/header.component.ts
+++ src/app/sub/header/header.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
+import {interval, Subscription} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscription: Subscription;
+  source = interval(5000);
+
+  currentState = 0;
+
+  keywords = [
+    'Intrapreneurship.',
+    'Selbstständigkeit.',
+    'Geld.'
+  ];
+
+  constructor(@Inject(DOCUMENT) document) { }
 
   ngOnInit() {
+    this.subscription = this.source.subscribe(val => this.switch());
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  switch() {
+    document.getElementById('keyword-switch').innerText = this.keywords[this.currentState];
+    this.currentState = (this.currentState + 1) % 3;
+  }
 }
