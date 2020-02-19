3eea653caeab8dd4c758fd533ca7bcf57e149b8b
Qu1oX <danny1996@gmx.net>
2020-02-19 20:31:42 +0100

    Added login functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 $('.dropdown-menu').click(function(e) {
     e.stopPropagation();
 });
+
+function executeLogin() {
+    var request = new XMLHttpRequest()
+    var email = document.getElementById("input-email").value;
+
+    if(email.length <= 0) {
+        document.getElementById("login-required-message").className = "required";
+        return;
+    } else {
+        document.getElementById("login-required-message").className = "hide";
+    }
+
+    request.open('POST', 'https://clerkvest.com/api/login?mail=' + email, true)
+    
+    request.onload = function() {
+
+        var data = this.response;
+        var responseField = document.getElementById("login-response-message");
+
+        console.log(responseField);
+
+        if (request.status >= 200 && request.status < 400) {
+            responseField.innerHTML = "Email send successfully. It could happen that our email lands inside of your spam folder."
+            responseField.className = "success";
+        } else {
+            responseField.innerHTML = "Failed to send email. Please contact our support if you continuously getting this error message.";
+            responseField.className = "failure";
+        }
+    }
+    
+    request.send();
+}
