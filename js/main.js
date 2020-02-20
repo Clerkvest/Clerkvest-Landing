c7047200ce80ec5d187565be979864e390ccf039
Qu1oX <danny1996@gmx.net>
2020-02-20 13:47:56 +0100

    Added company creation full setup functions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
+class Company {
+    constructor(domain, name, payAmount, payInterval, inviteOnly) {
+        this.domain = domain;
+        this.name = name;
+        this.payAmount = payAmount;
+        this.payInterval = payInterval;
+        this.inviteOnly = inviteOnly;
+    }
+}
+
+/**
+ * Onclick functin to keep the dropdown menu open
+ */
 $('.dropdown-menu').click(function(e) {
     e.stopPropagation();
 });
+
+/**
+ * Validates an email address
+ * @param {email} email 
+ * @returns {@code true} if the given email is valid {@code false} otherwise.
+ */
+function validateEmail(email)
+{
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if(email.match(mailformat)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 /**
  * Executes the login api call and sets the response field innerHtml
@@ -11,6 +40,10 @@ function executeLogin() {
 
     if(email.length <= 0) {
         document.getElementById("login-required-message").className = "required";
+        return;
+    } else if(!validateEmail(email)) {
+        document.getElementById("login-required-message").className = "required";
+        document.getElementById("login-required-message").innerHTML = "E-Mail address not valid"
         return;
     } else {
         document.getElementById("login-required-message").className = "hide";
@@ -45,6 +78,10 @@ function executeQuickSetup() {
     if(email.length <= 0) {
         document.getElementById("create-required-message-quick").className = "required";
         return;
+    } else if(!validateEmail(email)) {
+        document.getElementById("create-required-message-quick").className = "required";
+        document.getElementById("create-required-message-quick").innerHTML = "E-Mail address not valid"
+        return;
     } else {
         document.getElementById("create-required-message-quick").className = "hide";
     }
@@ -73,21 +110,79 @@ function executeQuickSetup() {
  */
 function executeFullSetup() {
     var request = new XMLHttpRequest()
-    var email = document.getElementById("input-email").value;
+    var email = document.getElementById("input-email-full").value;
+    var name = document.getElementById("input-name-full").value;
+    var payoutAmount = document.getElementById("input-payout-amount-full").value;
+    var payoutInterval = document.getElementById("input-payout-day-full").value;
+    var inviteOnly = document.getElementById("input-invite-only-full").checked;
+
+    var wasSomethingFalse = false;
 
     if(email.length <= 0) {
-        document.getElementById("login-required-message").className = "required";
-        return;
+        document.getElementById("create-required-message-email-full").className = "required";
+        document.getElementById("create-required-message-email-full").innerHTML = "This field is required"
+        wasSomethingFalse = true;
+    } else if(!validateEmail(email)) {
+        document.getElementById("create-required-message-email-full").className = "required";
+        document.getElementById("create-required-message-email-full").innerHTML = "E-Mail address not valid"
+        wasSomethingFalse = true;
     } else {
-        document.getElementById("login-required-message").className = "hide";
+        document.getElementById("create-required-message-email-full").className = "hide";
     }
 
-    request.open('POST', 'https://clerkvest.com/api/login?mail=' + email, true)
+    if(name.length <= 0) {
+        document.getElementById("create-required-message-name-full").className = "required";
+        document.getElementById("create-required-message-name-full").innerHTML = "This field is required"
+        wasSomethingFalse = true;
+    } else {
+        document.getElementById("create-required-message-name-full").className = "hide";
+    }
+
+    if(payoutAmount === undefined) {
+        document.getElementById("create-required-message-payout-amount-full").className = "required";
+        document.getElementById("create-required-message-payout-amount-full").innerHTML = "This field is required"
+        wasSomethingFalse = true;
+    } else if(payoutAmount <= 0) {
+        document.getElementById("create-required-message-payout-amount-full").className = "required";
+        document.getElementById("create-required-message-payout-amount-full").innerHTML = "Payout amount have to be at least one"
+        wasSomethingFalse = true;
+    } else {
+        document.getElementById("create-required-message-payout-amount-full").className = "hide";
+    }
+
+    if(payoutInterval === undefined) {
+        document.getElementById("create-required-message-payout-interval-full").className = "required";
+        document.getElementById("create-required-message-payout-interval-full").innerHTML = "This field is required"
+        wasSomethingFalse = true;
+    } else if(payoutInterval <= 0 || payoutInterval >= 31) {
+        document.getElementById("create-required-message-payout-interval-full").className = "required";
+        document.getElementById("create-required-message-payout-interval-full").innerHTML = "Payout interval have to be between one and thirty"
+        wasSomethingFalse = true;
+    } else {
+        document.getElementById("create-required-message-payout-interval-full").className = "hide";
+    }
+
+    if (wasSomethingFalse) {
+        return;
+    }
+
+    var domain = email.replace(/.*@/, "");
+
+    var companyObj = new Company(
+        domain,
+        name,
+        payoutAmount,
+        payoutInterval,
+        inviteOnly
+    );
+
+    request.open('POST', 'https://clerkvest.com/api/company/create?mail=' + email, true);
+    request.setRequestHeader("Content-type", "application/json")
     
     request.onload = function() {
 
         var data = this.response;
-        var responseField = document.getElementById("login-response-message");
+        var responseField = document.getElementById("create-response-message-full");
 
         if (request.status >= 200 && request.status < 400) {
             responseField.innerHTML = "Email send successfully. It could happen that our email lands inside of your spam folder."
@@ -98,5 +193,5 @@ function executeFullSetup() {
         }
     }
     
-    request.send();
+    request.send(companyObj);
 }
