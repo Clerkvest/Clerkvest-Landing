e8e8e231990f9a51e2a54c1b64c7a00b1b68d9f9
Qu1oX <danny1996@gmx.net>
2020-02-21 22:52:17 +0100

    Added up button

--- js/listeners.js
+++ js/listeners.js
@@ -4,3 +4,15 @@
 $('.dropdown-menu').click(function(e) {
     e.stopPropagation();
 });
+
+/**
+ * Onscroll function
+ */
+window.onscroll = function() {
+    if ((document.body.scrollTop > 20 && document.documentElement.clientWidth > 767) || 
+        (document.documentElement.scrollTop > 20 && document.documentElement.clientWidth > 767)) {
+        document.getElementById('up-button').style.display = 'block'
+    } else {
+        document.getElementById('up-button').style.display = 'none'
+    }
+};
