c2f8924b7692bc34b0211c136f26d2a2786ea9af
Qu1oX <danny1996@gmx.net>
2020-02-21 17:25:06 +0100

    Finished header design. Only missing text now

--- js/main.js
+++ js/main.js
@@ -6,8 +6,13 @@ function setImageMargin() {
     var heightRightTop = document.getElementById('header-img-right-top').clientHeight;
     var heightRightBottom = document.getElementById('header-img-right-bottom').clientHeight;
 
-    var margin = heightLeft - (heightRightTop + heightRightBottom);
-    document.getElementById('header-img-right-bottom').style.marginTop = margin + 'px';
+    if (document.documentElement.clientWidth > 767) { 
+        var margin = heightLeft - (heightRightTop + heightRightBottom);
+        document.getElementById('header-img-right-bottom').style.marginTop = margin + 'px';
+    } else {
+        var margin = 25;
+        document.getElementById('header-img-right-bottom').style.marginTop = margin + 'px';
+    }
 }
 
 /**
