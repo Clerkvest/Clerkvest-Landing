13fa21e888fe3415a538a89c0bec878f6ee028f0
Qu1oX <danny1996@gmx.net>
2020-02-22 12:48:10 +0100

    Navbar is now sticky. Removed up button for now

--- js/listeners.js
+++ js/listeners.js
@@ -33,15 +33,3 @@ function redirectIfLoggedIn() {
         window.location.href = '/app';
     }
 }
-
-/**
- * Onscroll function
- */
-window.onscroll = function() {
-    if ((document.body.scrollTop > 20 && document.documentElement.clientWidth > 767) || 
-        (document.documentElement.scrollTop > 20 && document.documentElement.clientWidth > 767)) {
-        document.getElementById('up-button').style.display = 'block'
-    } else {
-        document.getElementById('up-button').style.display = 'none'
-    }
-};
