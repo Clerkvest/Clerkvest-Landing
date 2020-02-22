6f3399e64129daa618f72744b170714506423b47
Qu1oX <danny1996@gmx.net>
2020-02-22 10:18:52 +0100

    Added function to redirect when logged in already

--- js/listeners.js
+++ js/listeners.js
@@ -6,6 +6,39 @@ $('.dropdown-menu').click(function(e) {
 });
 
 /**
+ * Reads the value of a cookie. Code by: https://www.w3schools.com/js/js_cookies.asp
+ * @param {String} name Name of the Cookie
+ */
+function readCookie(name) {
+    var nameEQ = name + '=';
+    var ca = document.cookie.split(';');
+
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+
+    return undefined;
+}
+
+/**
+ * Redirects of cookies are set
+ */
+function redirectIfLoggedIn() {
+    var api = readCookie('api');
+    var myself = readCookie('myself');
+
+    console.log(api);
+    console.log(myself);
+
+    if (api != undefined && myself != undefined) {
+        console.log("redirecting");
+        window.location.href = '/api';
+    }
+}
+
+/**
  * Onscroll function
  */
 window.onscroll = function() {
