68b964a26d7f95bff5068a27ec49548db13f45a1
Qu1oX <danny1996@gmx.net>
2020-02-23 18:16:15 +0100

    Website now deletes api and myself cookie if unvalid.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
 /**
+ * Reads the value of a cookie. Code by: https://www.w3schools.com/js/js_cookies.asp
+ * @param {String} name Name of the Cookie
+ */
+function readCookie(name) {
+    var nameEQ = name + '=';
+    var ca = document.cookie.split(';');
+
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+
+    return undefined;
+}
+
+/**
+ * Delete a cookie. Code by: https://www.codexpedia.com/javascript/javascript-create-read-and-delete-cookies/
+ * @param {String} cname, cookie name
+ */
+function deleteCookie(cname) {
+    var d = new Date();
+    d.setTime(d.getTime() - (1000*60*60*24));
+    var expires = "expires=" + d.toGMTString();
+    window.document.cookie = cname+"="+"; "+expires;
+ 
+}
+
+/**
  * Sets the margin of the bottom right image
  */
 function setImageMargin() {
@@ -16,8 +45,33 @@ function setImageMargin() {
 }
 
 /**
+ * Deletes cookies if token is old to prevent auto login into an old account.
+ */
+function deleteCookieIfOld() {
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'https://clerkvest.com/api/employee/get', true)
+    
+    request.onload = function() {
+
+        var data = this.response;
+
+        if (request.status == 401) {
+            deleteCookie('api');
+            deleteCookie('myself');
+        }
+
+        console.log(request.status);
+    }
+    
+    request.setRequestHeader('Authorization', 'Bearer '  + readCookie('api'));
+    request.send();
+}
+
+/**
  * Main function to init everything
  */
 function main() {
     setImageMargin();
+    deleteCookieIfOld();
 }
